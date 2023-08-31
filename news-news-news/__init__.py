8c70a21f6025719b0097f2f9adba346e6b969323
Matteo <matteo-devel@proton.me>
2023-08-31 18:07:00 +0200

    able to download tg1

--- news-news-news/__init__.py
+++ news-news-news/__init__.py
@@ -1,5 +1,4 @@
-#! /usr/bin/python3
-import pygooglenews, goose3, requests, re, curses, html, fake_useragent
+import pygooglenews, goose3, requests, re, html, fake_useragent, youtube_dl
 
 NEWSPAPERS = [
         "https://www.corriere.it",
@@ -9,50 +8,75 @@ NEWSPAPERS = [
         "https://tg24.sky.it",
         "https://www.open.online",
         "https://www.rainews.it"
-        ]
+    ]
 
-def _get_JSESSIONID_value(url, headers) -> str:
+def _get_latest_tg1_webpage_url() -> str:
+    r = requests.get("https://www.rainews.it/notiziari/tg1")
+    r.raise_for_status()
+
+    # get sign-language news section
+    match = re.search(
+        r'<rainews-aggregator-expandable data=\'{\"title\":\"Edizioni L.i.s.\"(.*?)</rainews-aggregator-expandable>',
+        html.unescape(r.text)
+    )
+    if match == None:
+        raise
+    match = match.group(1)
+
+    # get latest tg1 webpage url
+    match = re.search(r"\/notiziari(.*?).html", match)
+    if match == None:
+        raise
+    return "https://www.rainews.it" + match.group(0)
+
+
+# get valid JSESSIONID from the server to get the m3u8 url from
+def _get_JSESSIONID_value(url: str, headers: dict) -> str:
     s = requests.Session()
     s.get(url=url, headers=headers)
     cookies = s.cookies.get_dict()
     s.close()
-    print(cookies)
-    print(cookies["JSESSIONID"])
-    return  cookies["JSESSIONID"]
+    return cookies["JSESSIONID"]
 
-# return the link to download the last sign-language video news
-def get_tg1_download_url():
 
-    r = requests.get("https://www.rainews.it/notiziari/tg1")
+def get_tg1_m3u8_url(url: str):
+    # get escaped html of the latest tg1 webpage
+    r = requests.get(url)
+    r.raise_for_status()
 
-    # get sign-language news section
-    match = re.search(r"<rainews-aggregator-expandable data=\'{&quot;title&quot;:&quot;Edizioni L.i.s.&quot;(.*?)</rainews-aggregator-expandable>", r.text)
-    if match == None: raise
-    match = match.group(1)
+    # search the url to get m3u8 url from
+    match = re.search(
+        r"<rainews-player data(.*?)</rainews-player", html.unescape(r.text)
+    )
+    if match == None:
+        raise
+    match = re.search(r"http(.*?)\"", match.group(1))
+    if match == None:
+        raise
 
-    # get last news webpage url
-    match = re.search(r"\/notiziari(.*?).html", match)
-    if match == None: raise
+    # remove trailing quote and add parameter
+    url = match.group(0)[:-1] + "&output=61"
 
-    requests.get("https://www.rainews.it" + match.group(0))
-
-    # search the url to download the video from
-    match = re.search(r"http:\/\/mediapolisvod(.*?)&quot;", r.text)
-    if match == None: raise
-
-    # unescape the match, remove trailing quote and add parameter
-    url = html.unescape(match.group(0))[:-1] + "&output=61"
-    print(url)
-
-    # https://stackoverflow.com/questions/27652543/how-to-use-python-requests-to-fake-a-browser-visit-a-k-a-and-generate-user-agent
-    headers = {'User-Agent' : str(fake_useragent.UserAgent().chrome)}
-    # cookies = {'JSESSIONID' : 'ZjUur+3RP8e7N3SDEVFDbkLu'}
-    cookies = {'JSESSIONID' : _get_JSESSIONID_value(url, headers)}
+    headers = {"User-Agent": str(fake_useragent.UserAgent().chrome)}
+    cookies = {"JSESSIONID": _get_JSESSIONID_value(url, headers)}
 
     r = requests.get(url, cookies=cookies, headers=headers)
-    print(r.text)
+    match = re.search(r"<!\[CDATA\[(.*?)]", r.text)
+    if match == None:
+        raise
+    return match.group(1)
+
+
 def get_tg1():
-    pass
+    m3u8 = get_tg1_m3u8_url(_get_latest_tg1_webpage_url())
+
+    # https://stackoverflow.com/questions/69517841/how-to-set-youtube-dl-python-to-always-pick-1080p
+    ydl_opts = {
+        "outtmpl": "tg1.mp4",
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best",
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([m3u8])
 
 def get_news(newspapers = NEWSPAPERS):
     gn = pygooglenews.GoogleNews(lang = "it", country = "IT")
@@ -75,5 +99,4 @@ def get_news(newspapers = NEWSPAPERS):
             })
         input()
 
-get_tg1_download_url()
-#get_news()
+get_tg1()
